13493e04307bb6ad5fde8fd23bcb25187cb1da71
Angel Rey <afreydev@gmail.com>
2023-06-13 22:24:25 -0500

    added k8s

--- flask-backend/api/run.sh
+++ flask-backend/api/run.sh
@@ -2,7 +2,7 @@ if [ "${APP_ENV}" = "PROD" ]
 then
     gunicorn --bind 0.0.0.0:5000 --limit-request-field_size 16380 wsgi:application
 else
-    export FLASK_ENV=development
+    export FLASK_DEBUG=1
     export FLASK_APP=app.py
     flask run --host=0.0.0.0
 fi
